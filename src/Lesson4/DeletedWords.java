e54bab8e86bde4476e8243799063902c6effd025
NeuroToy <tsukimamoru@gmail.com>
2016-11-18 15:45:55 +0300

    added

--- src/Lesson4/DeletedWords.java
+++ src/Lesson4/DeletedWords.java
@@ -6,7 +6,14 @@ public class DeletedWords {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Type your text:");
 		Scanner scan = new Scanner(System.in);
+		String text = scan.nextLine();
+		System.out.println("How long words your prefer to delete (number):");
+		Scanner scanInt = new Scanner(System.in);
+		String number = scanInt.nextLine();
+		
 		scan.close();
+		scanInt.close();
 	}
 }
