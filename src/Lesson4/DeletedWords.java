e033a578cf57ad8f4b0b00b7c7d0fc465ac98a47
NeuroToy <tsukimamoru@gmail.com>
2016-11-20 15:08:01 +0200

    Add task 3 less 4

--- src/Lesson4/DeletedWords.java
+++ src/Lesson4/DeletedWords.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DeletedWords {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Type your text:");
 		Scanner scan = new Scanner(System.in);
 		String text = scan.nextLine();
@@ -13,7 +13,8 @@ public class DeletedWords {
 		System.out.println("How long words your prefer to delete (number):");
 		Scanner scanInt = new Scanner(System.in);
 		String secondScan = scanInt.nextLine();
-		char[] smb = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
+		char[] smb = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
+				'z' };
 		String textNew = new String();
 		Integer number = new Integer(secondScan);
 		String[] textParts = text.split(" ");
