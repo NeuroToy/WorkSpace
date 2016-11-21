bb35df0356f07bc844c740a10560793e4f106bef
NeuroToy <tsukimamoru@gmail.com>
2016-11-21 13:14:49 +0300

    Full task 5

--- src/Lesson4/Inversion.java
+++ src/Lesson4/Inversion.java
@@ -1,17 +1,32 @@
 package Lesson4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Inversion {
 
 	public static void main(String[] args) {
 
-		System.out.println("Type your text:");
+		System.out.println("Type your string:");
 		Scanner scan = new Scanner(System.in);
 		String text = scan.nextLine();
-		System.out.println("Type your text:");
+		String lowerText = text.toLowerCase();
+		String finalText = lowerText.replace(" ", "");
+		char[] array = finalText.toCharArray();
+		Arrays.sort(array);
+		System.out.println("Type your second string:");
 		Scanner secondScan = new Scanner(System.in);
 		String secondText = scan.nextLine();
+		String secondLower = secondText.toLowerCase();
+		String secondFinal = secondLower.replace(" ", "");
+		char[] secondArray = secondFinal.toCharArray();
+		Arrays.sort(secondArray);
+		if (Arrays.equals(array, secondArray)) {
+			System.out.println("Your second string is inversion of first");
+		} else {
+			System.out.println("Your second string is unique");
+		}
+		scan.close();
+		secondScan.close();
 	}
-
 }
