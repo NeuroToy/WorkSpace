4ac2bcc2462c30d5f7202d619108af53bc959e45
NeuroToy <tsukimamoru@gmail.com>
2016-11-21 07:34:07 +0200

    Task 5

---
+++ src/Lesson4/Inversion.java
@@ -0,0 +1,17 @@
+package Lesson4;
+
+import java.util.Scanner;
+
+public class Inversion {
+
+	public static void main(String[] args) {
+
+		System.out.println("Type your text:");
+		Scanner scan = new Scanner(System.in);
+		String text = scan.nextLine();
+		System.out.println("Type your text:");
+		Scanner secondScan = new Scanner(System.in);
+		String secondText = scan.nextLine();
+	}
+
+}
