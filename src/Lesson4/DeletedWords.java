7cc85c0c9fe818633310b51d5b31335c683b0494
NeuroToy <tsukimamoru@gmail.com>
2016-11-22 13:16:19 +0300

    Ver 1.2

--- src/Lesson4/DeletedWords.java
+++ src/Lesson4/DeletedWords.java
@@ -8,20 +8,21 @@ public class DeletedWords {
 
 		System.out.println("Type your text:");
 		Scanner scan = new Scanner(System.in);
-		String text = scan.nextLine();
-		int i = 0;
+		String in = scan.nextLine();
+		String text = in.toLowerCase();
+		String textNew = new String();
 		System.out.println("How long words your prefer to delete (number):");
 		Scanner scanInt = new Scanner(System.in);
 		String secondScan = scanInt.nextLine();
 		char[] smb = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
 				'z' };
-		String textNew = new String();
 		Integer number = new Integer(secondScan);
 		String[] textParts = text.split(" ");
 		for (String x : textParts) {
-			if (x.length() == number && x.charAt(0) == smb[i]) {
-				x = "";
-				i++;
+			for (char y : smb) {
+				if (x.length() == number && x.charAt(0) == y) {
+					x = "";
+				}
 			}
 			textNew += x + " ";
 		}
