2250366de505941e4a890089d74148d1f90a2197
NeuroToy <tsukimamoru@gmail.com>
2016-12-07 00:17:37 +0200

    Run update

--- src/Project/Run.java
+++ src/Project/Run.java
@@ -10,21 +10,21 @@ class Run {
 	public static void main(String[] args) {
 
 		LinkedHashSet<Car> taxiPark = new LinkedHashSet<Car>();
-		Minibus a = new Minibus("Mercedes V-250", 60600, 6.0, 9.1, 5, true);
-		taxiPark.add(a);
+		taxiPark.add(new Minibus("Mercedes V-250", 60600, 6.0, 9.1, 5, true));
 		taxiPark.add(new Minivan("Dodge Grand Caravan", 24590, 12.4, 10.8, 7, true));
 		taxiPark.add(new Pickup("Volkswagen Amarok", 25000, 7.9, 11.1, 4, 189));
 		taxiPark.add(new Cabrio("Mercedes E400", 63800, 9.4, 5.2, 3.0, "Leather"));
 		taxiPark.add(new Sedan("Audi A8 4.0 TFSI", 105060, 9.1, 4.5, 4.0, "Quattro"));
 		taxiPark.add(new Touring("BMW 550i GT", 112500, 9.6, 4.8, 4.4, "Tip-Tronic"));
 
-		System.out.println("1. Show the value of all park's cars:");
-		System.out.println("2. Show park's cars according to information:");
-		System.out.println("3. Exit");
 		Scanner scan = new Scanner(System.in);
+		Scanner secondScan = new Scanner(System.in);
+		boolean x = true;
 		try {
-			boolean x = true;
 			while (x) {
+				System.out.println("1. Show the value of all park's cars:");
+				System.out.println("2. Show park's cars according to information:");
+				System.out.println("3. Exit" + "\n");
 				int num = scan.nextInt();
 				switch (num) {
 				case 1:
@@ -34,15 +34,19 @@ class Run {
 						Car each = iterator.next();
 						costOfPark += each.price;
 					}
-					System.out.println(costOfPark + "$");
+					System.out.println("\n" + costOfPark + "$" + "\n");
 					break;
 				case 2:
-					System.out.println("1. Test");
-					System.out.println("2. Test");
-					switch (num) {
-					case 1:				
+					System.out.println("\n" + "1. Test");
+					System.out.println("2. Test" + "\n");
+					int secondNum = secondScan.nextInt();
+					switch (secondNum) {
+					case 1:
+						System.out.println("\n" + "2. Test1.1");
+						break;
 					case 2:
-						System.out.println("2. Test1.2");
+						System.out.println("\n" + "2. Test1.2" + "\n");
+						break;
 					}
 					break;
 				default:
@@ -50,11 +54,13 @@ class Run {
 						x = false;
 						System.exit(num);
 					}
-					System.out.println("Press 1-3");
+					System.out.println("\n" + "Press 1-3" + "\n");
 				}
 			}
 		} catch (InputMismatchException e) {
-			System.out.println("Please, enter numbers");
+			System.out.println("\n" + "Not a number" + "\n");
 		}
+		scan.close();
+		secondScan.close();
 	}
 }
