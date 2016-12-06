f082fa61e28f0a9f845939e3b1aaa7b1993abdd8
NeuroToy <tsukimamoru@gmail.com>
2016-12-07 00:51:02 +0200

    Update run

--- src/Project/Run.java
+++ src/Project/Run.java
@@ -1,26 +1,25 @@
 package Project;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 class Run {
 
 	public static void main(String[] args) {
 
-		LinkedHashSet<Car> taxiPark = new LinkedHashSet<Car>();
+		ArrayList<Car> taxiPark = new ArrayList<Car>();
 		taxiPark.add(new Minibus("Mercedes V-250", 60600, 6.0, 9.1, 5, true));
 		taxiPark.add(new Minivan("Dodge Grand Caravan", 24590, 12.4, 10.8, 7, true));
 		taxiPark.add(new Pickup("Volkswagen Amarok", 25000, 7.9, 11.1, 4, 189));
 		taxiPark.add(new Cabrio("Mercedes E400", 63800, 9.4, 5.2, 3.0, "Leather"));
 		taxiPark.add(new Sedan("Audi A8 4.0 TFSI", 105060, 9.1, 4.5, 4.0, "Quattro"));
 		taxiPark.add(new Touring("BMW 550i GT", 112500, 9.6, 4.8, 4.4, "Tip-Tronic"));
-
 		Scanner scan = new Scanner(System.in);
 		Scanner secondScan = new Scanner(System.in);
-		
 		boolean x = true;
+
 		try {
 			while (x) {
 				System.out.println("1. Show the value of all park's cars:");
@@ -63,10 +62,9 @@ class Run {
 					}
 					System.out.println("\n" + "Press 1-3" + "\n");
 				}
-				
 			}
 		} catch (InputMismatchException e) {
-			System.out.println("\n" + "Not a number" + "\n");
+			System.out.println("\n" + "Not a number");
 		}
 		scan.close();
 		secondScan.close();
