027da04d97fb084f833c43f8ad0d627db618463a
user <user@192.168.101.111>
2016-12-08 20:12:30 +0300

    Update Run

--- src/Project/Run.java
+++ src/Project/Run.java
@@ -9,13 +9,14 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 class Run {
 
 	public static void main(String[] args) {
 
-		ArrayList<Car> taxiPark = new ArrayList<Car>();
+		List<Car> taxiPark = new ArrayList<Car>();
 		taxiPark.add(new Minibus("Mercedes V-250", 60600, 9.1, 6.0, 5, true));
 		taxiPark.add(new Minivan("Dodge Grand Caravan", 24590, 10.8, 12.4, 7, true));
 		taxiPark.add(new Pickup("Volkswagen Amarok", 25000, 11.1, 7.9, 4, 189));
