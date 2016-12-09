7c96a158f3943f68c2a12cd1f977d8b39431f982
NeuroToy <tsukimamoru@gmail.com>
2016-12-09 13:01:46 +0300

    Documentation comments added

--- src/Project/Car.java
+++ src/Project/Car.java
@@ -2,14 +2,32 @@ package Project;
 
 import java.io.Serializable;
 
+/**
+ * Класс служит для хранения объектов со свойствами <br>
+ * <b>carName</b> <br>
+ * <b>price</b> <br>
+ * <b>acceleration</b> <br>
+ * <b>fuelConsumption</b>
+ * 
+ * @author malevich
+ */
 public class Car implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	/** Свойство - имя */
 	private String carName;
+	/** Свойство - цена */
 	private int price;
+	/** Свойство - ускорение */
 	private double acceleration;
+	/** Свойство - расход топлива */
 	private Double fuelConsumption;
 
+	/**
+	 * Функция для получения значения поля {@link Car#carName}
+	 * 
+	 * @return Имя машины
+	 */
 	public String getCarName() {
 		return carName;
 	}
@@ -18,6 +36,11 @@ public class Car implements Serializable {
 		this.carName = carName;
 	}
 
+	/**
+	 * Функция для получения значения поля {@link Car#price}
+	 * 
+	 * @return цена
+	 */
 	public int getPrice() {
 		return price;
 	}
@@ -26,6 +49,11 @@ public class Car implements Serializable {
 		this.price = price;
 	}
 
+	/**
+	 * Функция для получения значения поля {@link Car#acceleration}
+	 * 
+	 * @return ускорение
+	 */
 	public double getAcceleration() {
 		return acceleration;
 	}
@@ -34,10 +62,31 @@ public class Car implements Serializable {
 		this.acceleration = acceleration;
 	}
 
+	/**
+	 * Функция для получения значения поля {@link Car#fuelConsumption}
+	 * 
+	 * @return расход топлива
+	 */
+	public Double getFuelConsumption() {
+		return fuelConsumption;
+	}
+
 	public void setFuelConsumption(Double fuelConsumption) {
 		this.fuelConsumption = fuelConsumption;
 	}
 
+	/**
+	 * Создает новый объект с заданными значениями
+	 * 
+	 * @param carName
+	 *            имя
+	 * @param price
+	 *            цена
+	 * @param acceleration
+	 *            ускорение
+	 * @param fuelConsumption
+	 *            расход топлива
+	 */
 	public Car(String carName, int price, double acceleration, Double fuelConsumption) {
 		this.carName = carName;
 		this.price = price;
@@ -45,10 +94,10 @@ public class Car implements Serializable {
 		this.fuelConsumption = fuelConsumption;
 	}
 
-	public Double getFuelConsumption() {
-		return fuelConsumption;
-	}
-
+	/**
+	 * @return строку с заданными значениями
+	 */
+	@Override
 	public String toString() {
 		return carName + "\nCar price: " + price + "$" + "\nAcceleration: " + acceleration + "\nFuel consumption: "
 				+ fuelConsumption;
