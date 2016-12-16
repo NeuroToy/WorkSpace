f054d63efd62589c10e3283353dfcda55134afb5
NeuroToy <tsukimamoru@gmail.com>
2016-12-16 15:42:41 +0300

    Plant updated

--- src/TaskModule2/Plant.java
+++ src/TaskModule2/Plant.java
@@ -2,31 +2,37 @@ package TaskModule2;
 
 public class Plant {
 
-	private static String name;
-	private static String soil;
-	private static String origin;
+	private String name;
+	private String soil;
+	private String origin;
 
-	public static String getName() {
+	public String getName() {
 		return name;
 	}
 
-	public static void setName(String name) {
-		Plant.name = name;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public static String getSoil() {
+	public String getSoil() {
 		return soil;
 	}
 
-	public static void setSoil(String soil) {
-		Plant.soil = soil;
+	public void setSoil(String soil) {
+		this.soil = soil;
 	}
 
-	public static String getOrigin() {
+	public String getOrigin() {
 		return origin;
 	}
 
-	public static void setOrigin(String origin) {
-		Plant.origin = origin;
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	
+	public Plant (String name, String soil, String origin) {
+		this.name = name;
+		this.origin = origin;
+		this.soil = soil;
 	}
 }
