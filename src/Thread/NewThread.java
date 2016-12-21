fa85747e14dd1e81c7465cbc4199630b546451de
NeuroToy <tsukimamoru@gmail.com>
2016-12-21 11:59:20 +0300

    Threat Done!

--- src/Thread/NewThread.java
+++ src/Thread/NewThread.java
@@ -3,30 +3,29 @@ package Thread;
 class NewThread extends Thread {
 	boolean suspeandFlag;
 	int count;
+	Thread t;
 
 	public NewThread(int num) {
-		super("Thread" + num + ": ");
+		super("Thread" + num);
+		suspeandFlag = false;
 	}
 
 	public void run() {
-		System.out.print("Thread: ");
+		System.out.print("\n" + Thread.currentThread().getName() + ": ");
 		try {
 			for (count = 1; count < 101; count++) {
-				System.out.print(count);
-
-				while (suspeandFlag) {
-					wait();
+				System.out.print(count + " ");
+				synchronized (this) {
+					if (count % 10 == 0) {
+						suspeandFlag = true;
+						while (suspeandFlag && count != 100) {
+							wait();
+						}
+					}
 				}
-
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
-	}
-
-	synchronized void mysuspend() {
-		if (count % 10 == 0) {
-			suspeandFlag = true;
 		}
 	}
 
--- src/Thread/ThreadMain.java
+++ src/Thread/ThreadMain.java
@@ -4,9 +4,27 @@ public class ThreadMain {
 
 	public static void main(String[] args) {
 
-		Thread first = new NewThread(1);
-		Thread second = new NewThread(2);
-		first.start();
-		second.start();
+		NewThread first = new NewThread(1);
+		NewThread second = new NewThread(2);
+
+		try {
+			first.start();
+			Thread.sleep(200);
+			second.start();
+			while (first.count < 100) {
+				Thread.sleep(200);
+				System.out.print("\n" + first.getName() + ": ");
+				Thread.sleep(200);
+				first.myresume();
+				Thread.sleep(200);
+				System.out.print("\n" + second.getName() + ": ");
+				Thread.sleep(200);
+				second.myresume();
+			}
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\nExit.");
 	}
 }
